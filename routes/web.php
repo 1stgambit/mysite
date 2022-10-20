bdb413da8e0de508f7836267c67a042637af2c5b
Andrey <1stgambit@tut.by>
2022-10-20 16:56:37 +0300

    message_11

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,9 @@ Route::any('/login/vk/callback', [Controllers\AuthController::class, 'vkGet']);
 
 Route::any('/login/google', [Controllers\AuthController::class, 'googlePut']);
 Route::any('/login/google/callback', [Controllers\AuthController::class, 'googleGet']);
+
+
+
+
+Route::any('/login/googleeee', [Controllers\AuthController::class, 'googlePut']);
+Route::any('/login/googlessee', [Controllers\AuthController::class, 'googlePut']);
